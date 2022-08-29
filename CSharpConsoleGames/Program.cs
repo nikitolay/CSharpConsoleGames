4b415e56a8f38975dff4d334a4604093fe53068c
Nikolay Pavlov <npavlovit@gmail.com>
2022-08-29 22:47:55 +0300

    Add Visual Studio files

---
+++ CSharpConsoleGames/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpConsoleGames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
